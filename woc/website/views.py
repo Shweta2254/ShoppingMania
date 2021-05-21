bab09a9ab6977ff4cec1edd00d1adbaf7457b239
Shweta Srivastava <81436968+Shweta2254@users.noreply.github.com>
2021-05-21 20:31:15 +0530

    contact form changed

--- woc/website/views.py
+++ woc/website/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,HttpResponse
+from website.models import Contact
+
 
 # Create your views here.
 def home(request):
@@ -11,4 +13,11 @@ def offers(request):
     return render(request, 'offers.html')
 
 def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        message=request.POST.get('message')
+        contact = Contact(name=name, email=email, message=message)
+        contact.save()
+
     return render(request, 'contact.html')
