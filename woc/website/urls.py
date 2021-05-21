e4fc7c43895b7840a9c0c1cd281559394605cf5b
Shweta Srivastava <81436968+Shweta2254@users.noreply.github.com>
2021-05-21 15:37:39 +0530

    added navbar in contact page

--- woc/website/urls.py
+++ woc/website/urls.py
@@ -4,7 +4,9 @@ from website import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about', views.about, name='about'),
-    path('offers', views.offers, name='offers'),
-    path('contact', views.contact, name='contact')
+    path('home.html', views.home, name='home'),
+
+    path('about.html', views.about, name='about'),
+    path('offers.html', views.offers, name='offers'),
+    path('contact.html', views.contact, name='contact')
 ]
